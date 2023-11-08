4c432ed01211767851ef4804f1913092156dfaeb
Badr Ben m'barek <badr.benb@gmail.com>
2023-11-08 13:01:16 +0200

    chore(scrapy): add spider for new source of historical jumbles

--- jumble_scraper/jumble_scraper/items.py
+++ jumble_scraper/jumble_scraper/items.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass, asdict
+from pydantic import BaseModel, computed_field
+from urllib.parse import urlparse
+from datetime import datetime
 
 
 @dataclass
-class JumbleScraperItem:
+class JumbleSolverItem:
     url_from: str
     jumbled: str
     _unjumbled: str | None = None
@@ -25,3 +28,31 @@ class JumbleScraperItem:
         if exclude_fields:
             out = {u: v for u, v in out.items() if u not in exclude_fields}
         return out
+
+
+class JumbleAnswerItem(BaseModel):
+    jumbled: str
+    unjumbled: str
+
+
+class JumbleGameAnswerItem(BaseModel):
+    url_from: str
+    clue_sentence: str
+    solution_jumbled: str
+    solution_unjumbled: str
+    jumbles: list[JumbleAnswerItem]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @computed_field
+    @property
+    def value_date(self) -> str:
+        return self.extract_date_from_url(self.url_from)
+
+    @staticmethod
+    def extract_date_from_url(url):
+        path = urlparse(url).path
+        parts = path.strip("/").split("/")
+        date_string = "/".join(parts[:3])
+        return datetime.strptime(date_string, "%Y/%m/%d").strftime("%Y-%m-%d")
--- jumble_scraper/jumble_scraper/spiders/jumble_spider.py
+++ jumble_scraper/jumble_scraper/spiders/jumble_spider.py
@@ -4,29 +4,33 @@ from datetime import datetime, timedelta, date
 from typing import Any
 import scrapy
 
-from jumble_scraper.items import JumbleScraperItem
+from jumble_scraper.items import (
+    JumbleSolverItem,
+    JumbleGameAnswerItem,
+    JumbleAnswerItem,
+)
 
-RAW_URL = "https://jumblesolver.com"
-ANSWERS_ENDPOINT = "/daily-jumble-answers"
-DATE_FORMAT = "%Y-%m-%d"
 
-
-class JumbleSpiderSpider(scrapy.Spider):
-    name: str = "jumble_spider"
+class JumbleSolverSpider(scrapy.Spider):
+    name: str = "jumble_solver_spider"
 
     def __init__(
         self,
         end_date: str | date = date.today(),
+        raw_url: str = "https://jumblesolver.com",
+        answers_endpoint: str = "/daily-jumble-answers",
         date_offset: int = 10,
         output_path: str = "../../../data/jumble_data.json",
         force_update: bool = False,
+        date_format: str = "%Y-%m-%d",
         *args,
         **kwargs: Any,
     ):
         super().__init__(*args, **kwargs)
 
+        self.date_format = date_format
         if isinstance(end_date, str):
-            end_date = date.strptime(end_date, DATE_FORMAT)
+            end_date = date.strptime(end_date, self.date_format)
 
         self.end_date = end_date
         self.date_offset = int(date_offset)
@@ -34,8 +38,8 @@ class JumbleSpiderSpider(scrapy.Spider):
             force_update = eval(force_update)
         self.force_update = force_update
 
-        self.raw_url = RAW_URL
-        self.answer_endpoint = ANSWERS_ENDPOINT
+        self.raw_url = raw_url
+        self.answer_endpoint = answers_endpoint
         self.output_path = output_path
         self.visited_dates = self.load_value_dates_from_json()
 
@@ -50,7 +54,7 @@ class JumbleSpiderSpider(scrapy.Spider):
         for i in range(self.date_offset):
             value_date = self.end_date - timedelta(days=i)
             if (
-                value_date.strftime(DATE_FORMAT) not in self.visited_dates
+                value_date.strftime(self.date_format) not in self.visited_dates
                 or self.force_update
             ):
                 value_dates.append(value_date)
@@ -61,11 +65,11 @@ class JumbleSpiderSpider(scrapy.Spider):
 
     def start_requests(self):
         for value_date in self.value_dates:
-            url = f"{self.raw_url}{self.answer_endpoint}/{value_date.strftime(DATE_FORMAT)}"
+            url = f"{self.raw_url}{self.answer_endpoint}/{value_date.strftime(self.date_format)}"
             yield scrapy.Request(url, callback=self.parse_jumbles)
 
     def parse_jumble_solution(self, response):
-        item: JumbleScraperItem = response.meta["item"]
+        item: JumbleSolverItem = response.meta["item"]
         # the un-jambled word is the first button on the page
         button = response.css("button.word")[0]
         item.unjumbled = button.css("::text").extract()[0]
@@ -85,13 +89,118 @@ class JumbleSpiderSpider(scrapy.Spider):
             jumbled_endpoint = match.group(1)
             jumbled_url = f"{self.raw_url}{jumbled_endpoint}"
 
-            item = JumbleScraperItem(url_from=response.url, jumbled=jumbled)
+            item = JumbleSolverItem(url_from=response.url, jumbled=jumbled)
             yield scrapy.Request(
                 jumbled_url,
                 callback=self.parse_jumble_solution,
                 meta={"item": item},
                 dont_filter=True,
             )
+
+    def load_value_dates_from_json(self) -> list[str]:
+        try:
+            with open(self.output_path) as file:
+                visited_dates = [json.loads(line)["value_date"] for line in file]
+                return visited_dates
+        except FileNotFoundError:
+            return []
+
+    def save_to_json(self, data) -> None:
+        with open(self.output_path, "a") as file:
+            json.dump(data, file)
+            file.write("\n")
+
+
+class JumbleAnswersSpider(scrapy.Spider):
+    name: str = "jumble_answers_spider"
+
+    def __init__(
+        self,
+        end_date: str | date = date.today(),
+        raw_url: str = "https://jumbleanswer.com",
+        answers_endpoint_prefix: str = "/jumble-answers-for",
+        date_offset: int = 10,
+        output_path: str = "../../../data/jumble_answers_data.json",
+        force_update: bool = False,
+        date_format: str = "%Y-%m-%d",
+        *args,
+        **kwargs: Any,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.date_format = date_format
+        if isinstance(end_date, str):
+            end_date = date.strptime(end_date, self.date_format)
+
+        self.end_date = end_date
+        self.date_offset = int(date_offset)
+        if isinstance(force_update, str):
+            force_update = eval(force_update)
+        self.force_update = force_update
+
+        self.raw_url = raw_url
+        self.answers_endpoint_prefix = answers_endpoint_prefix
+        self.output_path = output_path
+        self.visited_dates = self.load_value_dates_from_json()
+
+        self.value_dates = self.get_value_dates()
+
+        self.logger.info(
+            f"Init. new spider with {len(self.value_dates)} dates to scrap (end_date={self.end_date} | force_update={self.force_update})"
+        )
+
+    def get_value_dates(self) -> list[datetime]:
+        value_dates = []
+        for i in range(self.date_offset):
+            value_date = self.end_date - timedelta(days=i)
+            if (
+                value_date.strftime(self.date_format) not in self.visited_dates
+                or self.force_update
+            ):
+                value_dates.append(value_date)
+        return value_dates
+
+    def parse(self, response):
+        pass
+
+    def start_requests(self):
+        for value_date in self.value_dates:
+            year = value_date.year
+            month = value_date.month
+            day = value_date.day
+            date_str = f"{month}-{day}-{value_date.strftime('%y')}"
+            url = f"{self.raw_url}/{year}/{str(month).zfill(2)}/{str(day).zfill(2)}/{self.answers_endpoint_prefix}-{date_str}"
+            yield scrapy.Request(url, callback=self.parse_jumbles)
+
+    def parse_jumbles(self, response):
+        content = ["".join(t.css("::text").extract()) for t in response.css("p")]
+
+        index_ref = content.index("CARTOON ANSWER:")
+        jumbles_text = content[:index_ref]
+        clue_sentence_text = content[index_ref + 1]
+        solution_text = content[index_ref + 2]
+
+        # parse jumbles
+        jumbles = []
+        for jumble_txt in jumbles_text:
+            jumbled, unjumbled = [t.strip() for t in jumble_txt.split("=")]
+            jumbles.append(
+                JumbleAnswerItem(**{"jumbled": jumbled, "unjumbled": unjumbled})
+            )
+
+        # parse the solution
+        jumbled_solution, unjumbled_solution = [
+            t.strip() for t in solution_text.split("=")
+        ]
+
+        jumble_game = JumbleGameAnswerItem(
+            url_from=response.url,
+            clue_sentence=clue_sentence_text.strip(),
+            solution_jumbled=jumbled_solution,
+            solution_unjumbled=unjumbled_solution,
+            jumbles=jumbles,
+        )
+        self.save_to_json(jumble_game.model_dump())
 
     def load_value_dates_from_json(self) -> list[str]:
         try:
