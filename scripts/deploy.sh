0eb95e0ef90af5179bbe97da6782047d35854134
Badr Ben m'barek <badr.benb@gmail.com>
2023-11-22 12:51:10 +0000

    chore(cloud): add cloud run deply script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-gcloud run deploy pastiche --image europe-west1-docker.pkg.dev/pastiche-405812/pastiche-repo/pastiche:latest
+gcloud run deploy pastiche --image europe-west1-docker.pkg.dev/pastiche-405812/pastiche-repo/pastiche:latest --allow-unauthenticated --max-instances=3 --region=europe-west1
